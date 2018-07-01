72003552d4aa59bd00ed6da48de7ffb904db334f
muthomi <muthomi.bryan@gmail.com>
2018-07-01 22:40:27 +0300

    actions and app completion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
+// Classes
+class API {
+  constructor(){
+    this.API_URL = 'https://free.currencyconverterapi.com/api/v5/';
+  }
+  getCurrencies(){
+    return fetch(this.API_URL+'currencies')
+      .then(res => res.json());
+  }
+  getConversionRate(query){
+    return fetch(this.API_URL+`convert?q=${query}&compact=y`)
+      .then(res => res.json());
+  }
+  getHistorical8dayRates(){}
+}
+
+
+// IndexDB using idb
+function openRatesDatabase(){
+  // If the browser doesn't support service worker,
+  // we don't care about having a database
+  if (!navigator.serviceWorker) {
+    return Promise.resolve();
+  }
+
+  return idb.open('ratesDatabase', 1, function(upgradeDb) {
+    const store = upgradeDb.createObjectStore('rates', {
+      keyPath: 'query'
+    });
+    store.createIndex('query','query');
+  });
+}
+openRatesDatabase();
+function openCurrenciesDatabase(){
+  if (!navigator.serviceWorker) {
+    return Promise.resolve();
+  }
+
+  return idb.open('currenciesDatabase', 1, function(upgradeDb) {
+    const currencyStore = upgradeDb.createObjectStore('currencies', {
+      keyPath: 'currencyName'
+    });
+  });
+}
+
+
+// SW - service worker
+function registerServiceWorker() {
+  if (!navigator.serviceWorker) return;
+  navigator.serviceWorker.register('/sw.js').then((reg) => {
+    if (!navigator.serviceWorker.controller) {
+      return;
+    }  
+  });
+}
+registerServiceWorker();
+
+// API
+const api = new API();
 // Chart js
 function chart8dayHistory(history, query) {
   const dataArray = [];
@@ -11,41 +70,6 @@ function chart8dayHistory(history, query) {
   });
   return dataArray;
 }
-const ctx = document.getElementById('8day-history').getContext('2d');
-const myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    label: ['USD_KES'],
-    datasets: [{
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(33, 33, 33, 0.2)',
-      ],
-      borderColor: [
-        'rgba(133,133,132,1)',
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    legend: {
-      display: false,
-    },
-    scales: {
-      yAxes: false
-    }
-  }
-});
-// SW - service worker
-function registerServiceWorker() {
-  if (!navigator.serviceWorker) return;
-  navigator.serviceWorker.register('/sw.js').then((reg) => {
-    if (!navigator.serviceWorker.controller) {
-      return;
-    }  
-  });
-}
-registerServiceWorker();
 
 // Store
 const createStore = (reducer) => {
@@ -69,35 +93,314 @@ const createStore = (reducer) => {
   return { getState, dispatch, subscribe }
 };
 const store = createStore(reducer);
+
+
 // Reducer
 function reducer(state = {
   type: 'INITIALIZATION',
-  sourceInput: null,
+  currencyList: {},
+  sourceInput: '',
   sourceCurrency: 'USD',
-  destinationInput: null,
+  destinationInput: '',
   destinationCurrency: 'KES',
   conversionQuery: 'USD_KES',
-  conversionRate: null,
-  pastConversions: [],
-  historicalRates: [],
-  notificationSourceInput: null,
-  notificationSourceCurrency: 'USD',
-  notificationDestinationInput: null,
-  notificationDestinationCurrency: 'KES',
-  currencyNotifications:[],
-  online: true
+  conversionRate: '',
+  conversionDate: '',
 }, action) {
+  const newState = Object.assign({},state);
   switch (action.type){
+  case 'ENTER_SOURCE_AMOUNT':
+    newState.type = action.type;
+    newState.sourceInput = action.payload.sourceInput;
+    newState.destinationInput = action.payload.destinationInput;
+    newState.conversionRate = action.payload.conversionRate;
+    newState.conversionDate = action.payload.conversionDate;
+    return newState;
+  case 'CHANGE_SOURCE_CURRENCY':
+    newState.type = action.type;
+    newState.sourceCurrency = action.payload.sourceCurrency;
+    newState.destinationInput = action.payload.destinationInput;
+    newState.conversionRate = action.payload.conversionRate;
+    newState.conversionDate = action.payload.conversionDate;
+    return newState;
+  case 'ENTER_DESTINATION_INPUT':
+    newState.type = action.type;
+    newState.sourceInput = action.payload.sourceInput;
+    newState.destinationInput = action.payload.destinationInput;
+    newState.conversionRate = action.payload.conversionRate;
+    newState.conversionDate = action.payload.conversionDate;
+    return newState;
+  case 'CHANGE_DESTINATION_CURRENCY':
+    newState.type = action.type;
+    newState.destinationCurrency = action.payload.destinationCurrency;
+    newState.destinationInput = action.payload.destinationInput;
+    newState.conversionRate = action.payload.conversionRate;
+    newState.conversionDate = action.payload.conversionDate;
+    return newState;
+  case 'SELECT_PAST_CONVERSION':
+    return newState;
+  case 'CHANGE_NOTIFICATION_SOURCE_AMOUNT':
+    return newState;
+  case 'CURRENCY_LIST_FETCHED':
+    newState.type = action.type;
+    newState.currencyList = action.payload.currencyList;
+    newState.conversionDate = action.payload.conversionDate;
+    return newState;
+  case 'SAVE_CONVERSION':
+    return newState;
+  case 'SAVE_NOTIFICATION':
+    return newState;
   default:
     return state;
   }
 }
 
+// UI hooks
+const sourceInput = document.getElementById('sourceInput');
+const sourceCurrency = document.getElementById('sourceCurrency');
+const destinationInput = document.getElementById('destinationInput');
+const destinationCurrency = document.getElementById('destinationCurrency');
+const dateOfConversion = document.getElementById('dateOfConversion');
+const pastConversionContainer = document.getElementById('pastConversionContainer');
+
+// Other variables
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// Render
+store.subscribe(render);
+function currencySelect(state, currencyDefault){
+  const currencyKeys = Object.keys(state.currencyList);
+  const currencyListOptionsHTML = currencyKeys.reduce((html, currencyKey) => {
+    return html + `<option value="${currencyKey}">${currencyKey} - ${state.currencyList[currencyKey].currencyName}</option>`;
+  },'');
+  return `<option value="${currencyDefault}">${currencyDefault} - ${state.currencyList[currencyDefault].currencyName}</option>` + currencyListOptionsHTML;
+}
+function render(){
+  const state = store.getState();
+  const year = state.conversionDate.getFullYear() || '';
+  const day = state.conversionDate.getDate() || '';
+  const month = state.conversionDate.getMonth() || '';
+  const date = `${months[month]}, ${day} ${year}` || 'date undefined';
+  console.log(state);
+  sourceInput.value = state.sourceInput;
+  sourceInput.style.minWidth = '6rem';
+  sourceInput.style.width = `${state.sourceInput.toString().length}rem`;
+  destinationInput.value = state.destinationInput;
+  destinationInput.style.minWidth = '6rem';
+  destinationInput.style.width = `${state.destinationInput.toString().length}rem`;
+  dateOfConversion.innerHTML = date;
+  sourceCurrency.innerHTML = currencySelect(state, state.sourceCurrency);
+  destinationCurrency.innerHTML = currencySelect(state, state.destinationCurrency);
+}
+function renderLists(){
+  const state = store.getState();
+  sourceCurrency.innerHTML = currencySelect(state, state.sourceCurrency);
+  destinationCurrency.innerHTML = currencySelect(state, state.destinationCurrency);
+}
+
+// Functions
+function storeCurrencies(results){
+  return openCurrenciesDatabase().then((db) => {
+    if(!db) return;
+    const tx = db.transaction('currencies','readwrite');
+    const currencyStore = tx.objectStore('currencies');
+    const currencyKeys = Object.keys(results);
+    currencyKeys.forEach((currencyKey) => {
+      currencyStore.put(results[currencyKey]);
+    });
+  });
+}
+function getStoredCurrencies(){
+  return openCurrenciesDatabase().then((db) => {
+    if(!db) return;
+    const tx = db.transaction('currencies');
+    const currencyStore = tx.objectStore('currencies');
+    return currencyStore.getAll().then((currencies) => {
+      const currencyObject = {};
+      currencies.map((currency) => {
+        return currencyObject[currency.id] = currency;
+      });
+      return store.dispatch({type: 'CURRENCY_LIST_FETCHED', 
+        payload: {
+          currencyList: currencyObject,
+          conversionDate: new Date()
+        } 
+      });
+    });
+  });
+}
+function storeRate(rate){
+  return openRatesDatabase().then((db) =>{
+    if (!db) return;
+    const tx = db.transaction('rates','readwrite');
+    const ratesStore = tx.objectStore('rates');
+    ratesStore.put(rate);
+    return tx.complete;
+  });
+}
+function conversion (rate, input, queryString){
+  const rateToStore = {query: queryString, val: rate[queryString].val};
+  storeRate(rateToStore);
+  return rate[queryString].val * input;
+}
+function query(source, dest, opposite=false){
+  if (!opposite){
+    return `${source}_${dest}`;
+  }
+  return `${dest}_${source}`;
+}
+function getStoredRates (queryString, actionType){
+  return openRatesDatabase().then((db) => {
+    if (!db) return;
+    const tx = db.transaction('rates');
+    const rateStore = tx.objectStore('rates');
+    const queryIndex = rateStore.index('query');
+    return queryIndex.get(queryString).then((rate) => {
+      if (!rate) return console.log('We currently don\'t have the rate for this transaction offline. Please try it online first');
+      const result = {[queryString]: {val: rate.val}};
+      switch (actionType) {
+      case 'ENTER_SOURCE_AMOUNT':
+        store.dispatch({
+          type: actionType,
+          payload: {
+            sourceInput: sourceInput.value,
+            destinationInput: conversion(result, sourceInput.value, queryString).toFixed(2).toLocaleString('en-US'),
+            conversionRate: result[queryString].val,
+            conversionDate: new Date()
+          }
+        });
+        break;
+      case 'ENTER_DESTINATION_AMOUNT':
+        store.dispatch({
+          type: 'ENTER_DESTINATION_INPUT',
+          payload: {
+            destinationInput: destinationInput.value,
+            sourceInput: conversion(result, destinationInput.value, queryString).toFixed(2).toLocaleString('en-US'),
+            conversionRate: result[queryString].val,
+            conversionDate: new Date()
+          }
+        });
+        break;
+      }
+    });
+  });
+}
 
 // Actions
-//TODO: Implement application actions.
-// idb
-//TODO: Implement the index db for the application
+window.addEventListener('load', () => {
+  api
+    .getCurrencies()
+    .catch(error => {
+      if ( !error.toString().includes('Failed to fetch')) return console.log(error);
+      getStoredCurrencies();
+      return;
+    })
+    .then((result) => {
+      if(!result) return;
+      store.dispatch({type: 'CURRENCY_LIST_FETCHED', 
+        payload: {
+          currencyList: result.results,
+          conversionDate: new Date()
+        } });
+      renderLists();
+      storeCurrencies(result.results);
+    });
+});
+document.getElementById('sourceInput')
+  .addEventListener('keyup', () => {
+    const queryString = query(sourceCurrency.value,destinationCurrency.value);
+    if (sourceInput.value) {
+      api
+        .getConversionRate(query(sourceCurrency.value,destinationCurrency.value))
+        .catch((error) => {
+          if ( !error.toString().includes('Failed to fetch')) return console.log(error);
+          getStoredRates(queryString,'ENTER_SOURCE_AMOUNT');
+          return;
+        })
+        .then((result) => {
+          if(!result) return;
+          store.dispatch({
+            type: 'ENTER_SOURCE_AMOUNT',
+            payload: {
+              sourceInput: sourceInput.value,
+              destinationInput: conversion(result, sourceInput.value, queryString).toFixed(2).toLocaleString('en-US'),
+              conversionRate: result[queryString].val,
+              conversionDate: new Date()
+            }
+          });
+        });
+    }
+  });
+sourceInput.addEventListener('click', () => {
+  sourceInput.value = '';
+});
+destinationInput.addEventListener('click', () => {
+  destinationInput.value = '';
+});
+document.getElementById('sourceCurrency')
+  .addEventListener('change', () => {
+    const queryString = query(sourceCurrency.value,destinationCurrency.value);
+    if (sourceInput.value) {
+      api
+        .getConversionRate(query(sourceCurrency.value,destinationCurrency.value))
+        .then((result) => {
+          store.dispatch({
+            type: 'CHANGE_SOURCE_CURRENCY',
+            payload: {
+              sourceCurrency: sourceCurrency.value,
+              destinationInput: conversion(result, sourceInput.value, queryString).toFixed(2).toLocaleString(),
+              conversionRate: result[queryString].val,
+              conversionDate: new Date()
+            }
+          });
+        });
+    }
+  });
+document.getElementById('destinationInput')
+  .addEventListener('keyup', () => {
+    const queryString = query(sourceCurrency.value,destinationCurrency.value,true);
+    if (destinationInput.value) {
+      api
+        .getConversionRate(query(sourceCurrency.value,destinationCurrency.value,true))
+        .catch((error) => {
+          if ( !error.toString().includes('Failed to fetch')) return console.log(error);
+          getStoredRates(queryString,'ENTER_DESTINATION_AMOUNT');
+          return;
+        })
+        .then((result) => {
+          if(!result) return;
+          
+          store.dispatch({
+            type: 'ENTER_DESTINATION_INPUT',
+            payload: {
+              destinationInput: destinationInput.value,
+              sourceInput: conversion(result, destinationInput.value, queryString).toFixed(2).toLocaleString('en-US'),
+              conversionRate: result[queryString].val,
+              conversionDate: new Date()
+            }
+          });
+        });
+    }
+  });
+document.getElementById('destinationCurrency')
+  .addEventListener('change', () => {
+    const queryString = query(sourceCurrency.value,destinationCurrency.value);
+    if (sourceInput.value) {
+      api
+        .getConversionRate(query(sourceCurrency.value,destinationCurrency.value))
+        .then((result) => {
+          store.dispatch({
+            type: 'CHANGE_DESTINATION_CURRENCY',
+            payload: {
+              destinationCurrency: destinationCurrency.value,
+              destinationInput: conversion(result, sourceInput.value, queryString).toFixed(2).toLocaleString('en-US'),
+              conversionRate: result[queryString].val,
+              conversionDate: new Date()
+            }
+          });
+        });
+    }
+  });
 // Online check
 const onlineStatus = document.getElementById('onlineStatus');
 if (navigator.onLine) {
---
+++ sw.js
@@ -0,0 +1,41 @@
+const staticCacheName = 'currency-converter-static';
+const allCaches = [
+  staticCacheName
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'js/idb.js',
+        'css/lux.css',
+        'style.css',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('currency-') &&
+                   !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
